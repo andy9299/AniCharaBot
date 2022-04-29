7a5c94cb1144f8a6e26977d8c326d2129e066ada
Andy <andy.tu998@gmail.com>
2022-04-29 16:31:07 -0400

    updated README and help command

--- discordCommands/help.js
+++ discordCommands/help.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'help',
     run: async ({client, message, args}) => {
-        message.reply("c.info [character name] - returns some information about a character")
+        message.reply("c.info [character name] - returns some information about a character\n" + 
+        "c.r [optional: number for popularity] - returns a random anime character within the top range of popularity (default top 15,000)")
     }
 }

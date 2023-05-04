3f60cd11b1de63835be0172520681bf1ccebfd1a
andy9299 <andy.tu998@gmail.com>
2023-05-04 15:02:42 -0400

    updated help command

--- discordCommands/help.js
+++ discordCommands/help.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: 'help',
-    run: async ({client, message, args}) => {
-        message.reply("c.info [character name] - returns some information about a character\n" + 
-        "c.r [optional: number for popularity] - returns a random anime character within the top range of popularity (default: top 15,000)")
+    run: async ({ client, message, args }) => {
+        message.reply("info [character name] - returns some information about a character\n" +
+            "r [optional: number for popularity] - returns a random anime character within the top range of popularity (default: top 15,000)\n" +
+            "g [optional: number for popularity] - returns an image of a random anime character where the user can guess who it is once (default: top 7,500)");
     }
-}
+};
